4f3abf547849ff46f2c61d90d7e66186a8a88210
RihanFoudeh <Rihanfoudeh@gmail.com>
2021-09-13 19:12:59 +0300

    fix lab12

--- src/Books.js
+++ src/Books.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'react-bootstrap';
+
 import { Card } from 'react-bootstrap';
 
 class Books extends Component {
@@ -8,22 +9,27 @@ class Books extends Component {
         console.log(this.props.booksArr);
         return (
             <div>
-               
+
+                <Carousel>
+                   
                 {this.props.booksArr.map(item => {
-                    return(
-                    < Card style={{ width: '18rem' }} className='card'>
-                        <Card.Body>
-                            <Card.Title>{item.bookTitle}</Card.Title>
-                            <Card.Text>
-                                {item.bookDescription}
-                            </Card.Text>
-                            <Card.Text>
-                            Availability : {item.bookStatus}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card >
-                    )
-                })}
+                     return(
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://www.cnet.com/a/img/zKiT35AfPlfRJvlHty6p4zmpCrw=/940x0/2020/05/01/7f0a951f-d9a2-4847-b962-b254b63842a2/img-4960.jpg"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>{item.bookTitle}</h3>
+                            <p> {item.bookDescription}</p>
+                            <h5> Availability : {item.bookStatus}</h5>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                      )
+                    })} 
+                </Carousel>
 
             </div>
         );
